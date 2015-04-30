5a49933356b54dc4933e07c50c1580e34ae17f24
Sil Westerveld <dev@silwesterveld.com>
2015-04-30 15:26:38 +0200

    In SQL do DROPs first, then do CREATEs. Implement deleteMatches, deletePlayers, countPlayers, registerPlayer and reportMatch in Python.

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -8,30 +8,72 @@ import psycopg2
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
-    return psycopg2.connect("dbname=tournament")
+    conn = psycopg2.connect("dbname=tournament")
+    return conn
 
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    query = "DELETE FROM matches;"
+
+    # Connect to the database
+    conn = connect()
+    # Open a cursur to perform database operations
+    cur = conn.cursor()
+    # Execute a command
+    cur.execute(query)
+    # Make the changes to the database persistent
+    conn.commit()
+    # Close communication with the database
+    conn.close()
+
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    query = "DELETE FROM players;"
+
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute(query)
+    conn.commit()
+    conn.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    query = "SELECT COUNT(*) FROM players;"
+
+    conn = psycopg2.connect("dbname=tournament")
+    cur = conn.cursor()
+    # Execute a command to query the database and obtain data as Python
+    # objects
+    cur.execute(query)
+    count = cur.fetchone()[0]
+    conn.commit()
+    cur.close()
+    conn.close()
+    return count
+
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
+    query = "INSERT INTO players (name) VALUES (%s);"
+
+    conn = connect()
+    cur = conn.cursor()
+    cur.execute(query, (name,))
+    conn.commit()
+    cur.close()
+    conn.close
 
 
 def playerStandings():
@@ -56,16 +98,19 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+    cursor = connect().cursor()
+    query = "INSERT INTO matches (p1, p2, winner) VALUES (%s)"
+    cursor.commit(query, (winner, loser, winner))
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -9,8 +9,13 @@
 
 DROP DATABASE IF EXISTS tournament;
 CREATE DATABASE tournament;
+DROP TABLE IF EXISTS players CASCADE;
+DROP TABLE IF EXISTS matches CASCADE;
+DROP VIEW IF EXISTS matches_per_player;
+DROP VIEW IF EXISTS wins_per_player;
+DROP VIEW IF EXISTS player_standings;
 
-DROP TABLE IF EXISTS players;
+
 CREATE TABLE players (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255)
@@ -22,9 +27,15 @@ CREATE TABLE tournaments (
 );
 */
 
-DROP TABLE IF EXISTS matches;
 CREATE TABLE matches (
     p1 INT REFERENCES players (id),
     p2 INT REFERENCES players (id),
     winner INT REFERENCES players (id)
 );
+
+-- Views --
+CREATE VIEW matches_per_player AS SELECT players.id AS id, COUNT(*) AS matches FROM players JOIN matches ON players.id = matches.p1 OR players.id = matches.p2 GROUP BY players.id ORDER BY players.id;
+
+CREATE VIEW wins_per_player AS SELECT players.id AS id, COUNT(matches.winner) AS wins FROM players LEFT JOIN matches ON players.id = matches.winner GROUP BY players.id ORDER BY players.id;
+
+-- CREATE VIEW player_standings AS SELECT
